5af8ced70c68e62333eff10ec883cc970f7c219e
Matej Hrescak <matej@hrescak.com>
2020-06-08 23:09:22 -0400

    fixed buttons

--- components/system/Button/PrimaryButton.tsx
+++ components/system/Button/PrimaryButton.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import baseStyle, {IconWrapper} from './base'
+import baseStyle, {IconWrapper, ButtonProps} from './base'
 import theme from '../theme'
 import React,{ ReactNode }  from 'react'
 
@@ -8,12 +8,8 @@ export const StylePrimaryButton = styled.a`
     background: ${theme.colors.control};
     color: ${theme.colors.controlText};
 `
-interface Props {
-    children?: ReactNode
-    icon?: ReactNode
-}
 
-export const PrimaryButton = React.forwardRef((props:Props, ref?: React.Ref<HTMLAnchorElement>) =>{
+export const PrimaryButton = React.forwardRef((props:ButtonProps, ref?: React.Ref<HTMLAnchorElement>) =>{
     return(
         <StylePrimaryButton ref={ref} {...props}>
             {props.icon &&
